f43c6c907aec00ff1775aa5889c50d090d2d51bd
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-10 12:02:45 -0500

    moved arrays off server.js. Created a album.js to store album. added an album route and response. Added a function in client.js to append a new table to the dom with ablums.

---
+++ server/modules/albums.js
@@ -0,0 +1,17 @@
+const albumListArray = [
+    {
+        title: 'St. Louis Blues',
+        year: 1925,
+    },
+    {
+        title: 'Plays Cole Porter',
+        year: 1951,
+    },
+    {
+        title: 'Lady Sings the Blues',
+        year: 1952,
+    }
+
+];
+
+module.exports = albumListArray;
--- server/public/client.js
+++ server/public/client.js
@@ -1,10 +1,31 @@
 $(document).ready(onReady);
 
+//runs everything on page load. 
+
 function onReady() {
+    //calls artistList function on page load. 
+   render();
+    // TODO Add ajax request for /songs and display on DOM
+}
+//function to render to DOM when called
+function render() {
+
+    artistList();
+    songList();
+    albumList();
+
+}
+
+function artistList() {
+    //using jquery to call ajax
+    //using ajax to get /artist from the server.
     $.ajax({
         type: 'GET',
         url: '/artist'
+        //using the .then method to use the response (artistList array )from the server 
+        //to loop through the artistListArray and using jquery to append to the table on the DOM 
     }).then(function (response) {
+        //takes the server "response" and loops through each item of the array
         for (let i = 0; i < response.length; i++) {
             let artist = response[i];
             $('#artistTableBody').append(`
@@ -16,6 +37,53 @@ function onReady() {
             `);
         }
     });
+}
+//created a function when called calls the server to get song data.
+//function render to DOM. 
+function songList() {
 
-    // TODO Add ajax request for /songs and display on DOM
+        $.ajax({
+            type: 'GET',
+            url: '/songs'
+
+        }).then(function (songs) {
+
+            for (let song of response ) {
+                $('#songTableBody').append(`
+                    <tr>
+                        <td>${song.title}</td>
+                        <td>${song.artist}</td>
+                    </tr>
+                `);
+            }
+        }).catch(function(err) {
+            alert('Request Failed');
+            console.log('Request failed', err)
+
+        })
+
+
+}
+//created a function when called calls the server to get album data.
+//function render to DOM.
+function albumList() {
+
+        $.ajax({
+            type: 'GET',
+            url: '/albums'
+        }).then(function(response) {
+            
+            for ( let album of response ) {
+                $('#albumTableBody').append(`
+                    <tr>
+                        <td>${album.title}</td>
+                        <td>${album.year}</td>
+                    </tr>
+                `);
+            }
+        }).catch(function(err) {
+            alert('Request Failed');
+            console.log('Request failed', err)
+
+        })
 }
